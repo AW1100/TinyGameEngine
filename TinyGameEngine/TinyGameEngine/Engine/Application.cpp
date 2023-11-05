8f6a023667174e9ad1f7c1042046dfbbc83f2c92
AW1100 <wanghaoran960112@gmail.com>
2023-11-05 15:02:11 -0500

    add start point and some math

--- TinyGameEngine/TinyGameEngine/Engine/Application.cpp
+++ TinyGameEngine/TinyGameEngine/Engine/Application.cpp
@@ -1,4 +1,5 @@
 #include "Application.h"
+#include "../Math/Math.h"
 #include <iostream>
 
 TGE::Application::Application()
@@ -11,9 +12,13 @@ TGE::Application::~Application()
 
 void TGE::Application::Run()
 {
+	FMatrix4 m;
+	FMatrix3 m2;
+	FVector3 v1;
+	FVector4 v2;
 	while (true)
 	{
-		std::cout << "hello world" << std::endl;
+		std::cout << m.m[3][3] << std::endl;
 	}
 }
 
--- TinyGameEngine/TinyGameEngine/Math/Vector.h
+++ TinyGameEngine/TinyGameEngine/Math/Vector.h
@@ -1,40 +1,61 @@
 #pragma once
 
-class Vector3
+namespace MathUtil
 {
-public:
-	Vector3(float _x = 0.0f, float _y = 0.0f, float _z = 0.0f) :
-		x(_x), y(_y), z(_z)
-	{}
-
-	Vector3 operator+(const Vector3& rhs) const
+	template <typename T>
+	class Vector3
 	{
-		return Vector3(x + rhs.x, y + rhs.y, z + rhs.z);
-	}
+	public:
+		Vector3() {}
+		Vector3(const Vector3& other)
+		{
+			x = other.x;
+			y = other.y;
+			z = other.z;
+		}
+		Vector3 operator+(const Vector3& rhs) const
+		{
+			return Vector3(x + rhs.x, y + rhs.y, z + rhs.z);
+		}
+		Vector3 operator-(const Vector3& rhs) const
+		{
+			return Vector3(x - rhs.x, y - rhs.y, z - rhs.z);
+		}
 
-	Vector3 operator-(const Vector3& rhs) const
+	public:
+		T x;
+		T y;
+		T z;
+	};
+
+	template <typename T>
+	class Vector4
 	{
-		return Vector3(x - rhs.x, y - rhs.y, z - rhs.z);
-	}
+	public:
+		Vector4() {}
+		Vector4(const class Vector3<T>& _v, T _w)
+		{
+			v = _v;
+			w = _w;
+		}
+		Vector4(const Vector4<T>& other)
+		{
+			v = other.v;
+			w = other.w;
+		}
+		Vector4 operator+(const Vector4<T>& rhs) const
+		{
+			return Vector4<T>(v + rhs.v, w + rhs.w);
+		}
+		Vector4 operator-(const Vector4<T>& rhs) const
+		{
+			return Vector4<T>(v - rhs.v, w - rhs.w);
+		}
 
-public:
-	float x;
-	float y;
-	float z;
-};
+	public:
+		class Vector3<T> v;
+		T w;
+	};
+}
 
-class Vector4
-{
-public:
-	Vector4(float _x = 0.0f, float _y = 0.0f, float _z = 0.0f) :
-		v(_x, _y, _z), w(1.0)
-	{}
 
-	Vector4(float _x, float _y, float _z, float _w) :
-		v(_x, _y, _z), w(_w)
-	{}
-
-public:
-	Vector3 v;
-	float w;
-};
