7c931aafd0d42c1b381f8266c8b08e2680509456
AW1100 <wanghaoran960112@gmail.com>
2024-01-15 17:29:08 -0500

    drawablebase

--- TinyGameEngine/TinyGameEngine/Engine/Drawable/Box.cpp
+++ TinyGameEngine/TinyGameEngine/Engine/Drawable/Box.cpp
@@ -1,5 +1,5 @@
 #include "Box.h"
-#include "../Bindable/Bases.h"
+#include "../Bindable/BindableHeader.h"
 
 
 Box::Box(Graphics& gfx, std::mt19937& rng, std::uniform_real_distribution<float>& adist, std::uniform_real_distribution<float>& ddist, std::uniform_real_distribution<float>& odist, std::uniform_real_distribution<float>& rdist)
